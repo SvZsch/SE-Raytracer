d939d3598ff22b1f259939afd18705405576eefe
SZ <sven.zschunke@web.de>
2024-09-08 12:36:51 +0200

    verbessern der Deklarationen für die Box Klasse, anpassung für bessere Schnitpunkt berechnungen und damit bessere Darstellung im Raytracer

--- framework/box.hpp
+++ framework/box.hpp
@@ -1,3 +1,4 @@
+// box.hpp
 #ifndef BOX_HPP
 #define BOX_HPP
 #include "shape.hpp"
@@ -5,16 +6,20 @@
 
 class Box : public Shape {
 private:
-	glm::vec3 min_{ 0.0f, 0.0f, 0.0f };
-	glm::vec3 max_{ 100.0f, 100.0f, 100.0f };
+    /*glm::vec3 min_{ 0.0f, 0.0f, 0.0f };
+    glm::vec3 max_{ 100.0f, 100.0f, 100.0f };*/
 public:
-	Box();
-	Box(glm::vec3 min, glm::vec3 max);
-	Box(glm::vec3 min, glm::vec3 max, std::string name, std::shared_ptr<Material> color);
-	float area()const;
-	float volume()const;
-	std::ostream& print(std::ostream& os)const;
-	Hitpoint intersect(Ray const& ray) override;
-};
+    Box();
+    Box(glm::vec3 min, glm::vec3 max);
+    Box(glm::vec3 min, glm::vec3 max, std::string const& name, std::shared_ptr<Material> const& color);
+    Box(glm::vec3 min, glm::vec3 max, std::shared_ptr<Material> const& material);
+    float area() const override;
+    float volume() const override;
+    std::ostream& print(std::ostream& os) const override;
 
+    glm::vec3 min_{ 0.0f, 0.0f, 0.0f };
+    glm::vec3 max_{ 100.0f, 100.0f, 100.0f };
+protected:
+    Hitpoint intersectImpl(Ray const& ray) const override;
+};
 #endif
