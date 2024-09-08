dadf325b4edd8a5b7876eb30a216a7c0333dc6ab
SZ <sven.zschunke@web.de>
2024-09-08 12:32:08 +0200

    fehler behbung bei der berechnung von Kugeln

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -3,121 +3,138 @@
 #include <numbers>
 #include <cmath>
 #include <glm/glm.hpp>
+#include <string>
+#include <sstream>
 #include <glm/gtx/intersect.hpp>
 #include <iostream>
 
-Sphere::Sphere():
-	Shape::Shape{"Kugel"},
+Sphere::Sphere() :
+    Shape{ "Kugel" },
     rad{ 10.0f },
-	point{ 100.0f, 100.0f, 100.0f }
-{std::cout << "Sphere ctor" << std::endl; }
+    point{ 100.0f, 100.0f, 100.0f }
+{
+    std::cout << "Sphere ctor" << std::endl;
+}
 
 Sphere::~Sphere()
 {
-	std::cout << "Sphere dtor" << std::endl;
+    std::cout << "Sphere dtor" << std::endl;
 }
 
 Sphere::Sphere(float r, glm::vec3 p) :
-	Shape::Shape{"Kugel"},
-	rad(r),
-	point(p)
+    Shape{ "Kugel" },
+    rad(r),
+    point(p)
 {
-	std::cout << "Sphere ctor" << std::endl;
+    std::cout << "Sphere ctor" << std::endl;
 }
 
-Sphere::Sphere(float r, glm::vec3 p, std::string name, std::shared_ptr<Material> color):
-	Shape::Shape{name, color},
-	rad(r),
-	point(p)
+Sphere::Sphere(float r, glm::vec3 p, std::string const& name, std::shared_ptr<Material> const& color) :
+    Shape{ name, color },
+    rad(r),
+    point(p)
 {
-	std::cout << "Sphere ctor" << std::endl;
+    std::cout << "Sphere ctor" << std::endl;
 }
 
-
-float Sphere::volume()const {
-	return abs((4.0f / 3.0f) * std::numbers::pi * rad * rad * rad);
+float Sphere::volume() const {
+    return abs((4.0f / 3.0f) * std::numbers::pi * rad * rad * rad);
 }
 
-float Sphere::area()const {
-	return abs(4.0f * std::numbers::pi * rad * rad);
+float Sphere::area() const {
+    return abs(4.0f * std::numbers::pi * rad * rad);
 }
 
-
-std::ostream& Sphere::print(std::ostream& os)const {
-	Shape::print(os);
-	return os << rad << " " << point.x << " " << point.y << " " << point.z << std::endl;
+std::ostream& Sphere::print(std::ostream& os) const {
+    Shape::print(os);
+    return os << rad << " " << point.x << " " << point.y << " " << point.z << std::endl;
 }
 
-Hitpoint Sphere::intersect(Ray const& ray) {
-	auto v = glm::normalize(ray.direction);
-	float dist = 0.0f;
+// Hilfsfunktion zum Konvertieren von glm::vec3 in einen String
+std::string vec3_to_string(const glm::vec3& v) {
+    std::stringstream ss;
+    ss << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+    return ss.str();
+}
 
-	/*if (glm::intersectRaySphere(ray.origin, v, point, rad * rad, dist)) {
-		Hitpoint hitpoint;
-		hitpoint.hit = true;
-		hitpoint.distance = dist;
-		hitpoint.name_obj = name_;
-		hitpoint.color_obj = color_;
+//Hitpoint Sphere::intersectImpl(Ray const& ray) const {
+//    std::cout << "Sphere::intersectImpl called for " << getName() << std::endl;
+//    std::cout << "Sphere center: (" << point.x << ", " << point.y << ", " << point.z << "), radius: " << rad << std::endl;
+//    std::cout << "Ray origin: (" << ray.origin.x << ", " << ray.origin.y << ", " << ray.origin.z
+//        << "), direction: (" << ray.direction.x << ", " << ray.direction.y << ", " << ray.direction.z << ")" << std::endl;
+//    
+//    auto v = glm::normalize(ray.direction);
+//    float dist = 0.0f;
+//    if (glm::intersectRaySphere(ray.origin, v, point, rad * rad, dist)) {
+//        Hitpoint hitpoint;
+//        hitpoint.hit = true;
+//        hitpoint.distance = dist;
+//        hitpoint.name_obj = name_;
+//        hitpoint.color_obj = color_;
+//
+//        const float EPSILON = 0.0001f;
+//        glm::vec3 intersection_point = ray.origin + (dist - EPSILON) * ray.direction;
+//        hitpoint.hit_p = intersection_point;
+//
+//        float n1 = 1.0f; // Brechungsindex von Luft
+//        float n2 = color_->refraction_index;
+//        float eta = n1 / n2;
+//        glm::vec3 normal = glm::normalize(hitpoint.hit_p - point);
+//
+//        if (glm::dot(ray.direction, normal) > 0) {
+//            // Strahl tritt aus der Kugel aus
+//            normal = -normal;
+//            std::swap(n1, n2);
+//            eta = n1 / n2;
+//        }
+//
+//        hitpoint.normal = normal;
+//        hitpoint.reflection_dir = glm::reflect(ray.direction, normal);
+//        hitpoint.refraction_dir = glm::refract(ray.direction, normal, eta);
+//
+//        if (hit) {
+//            std::cout << "Hit found at distance: " << dist << std::endl;
+//        }
+//        else {
+//            std::cout << "No hit found" << std::endl;
+//        }
+//
+//        return hitpoint;
+//    }
+//    else {
+//        return Hitpoint(); // Keine Kollision
+//    }
+//}
 
-		float a = (ray.direction.x) * (ray.direction.x) +
-			(ray.direction.y) * (ray.direction.y) + (ray.direction.z) * (ray.direction.z);
-		float b = 2 * ((ray.origin.x - point.x) * ray.direction.x +
-			+ (ray.origin.y - point.y) * ray.direction.y + (ray.origin.z - point.z) * ray.direction.z);
-		float c = ((ray.origin.x - point.x) * (ray.origin.x - point.x) +
-			(ray.origin.y - point.y) * (ray.origin.y - point.y) +
-			(ray.origin.z - point.z) * (ray.origin.z - point.z)) - (rad * rad);
-		std::cout << "hier " << a << " " << b << " " << c << std::endl;
+Hitpoint Sphere::intersectImpl(Ray const& ray) const {
+    glm::vec3 oc = ray.origin - point;
+    float a = glm::dot(ray.direction, ray.direction);
+    float b = 2.0f * glm::dot(oc, ray.direction);
+    float c = glm::dot(oc, oc) - rad * rad;
+    float discriminant = b * b - 4 * a * c;
 
-	
+    //std::cout << "a: " << a << ", b: " << b << ", c: " << c << std::endl;
+    //std::cout << "Discriminant: " << discriminant << std::endl;
 
-		if ((b * b - 4 * a * c) == 0) {
-			float t = (-b) / (2 * a);
-			glm::vec3 treffer = { ray.origin.x + (t * ray.direction.x), ray.origin.y + (t * ray.direction.y),
-			ray.origin.z + (t * ray.direction.z) };
-			hitpoint.hit_p = treffer;
-		}
-		else {
-			float t1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-			float t2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-			glm::vec3 treffer = { ray.origin.x + (t1 * ray.direction.x), ray.origin.y + (t1 * ray.direction.y),
-			ray.origin.z + (t1 * ray.direction.z) };
-			hitpoint.hit_p = treffer;
-			
+    Hitpoint hitpoint;
+    if (discriminant >= 0) {
+        float dist = (-b - std::sqrt(discriminant)) / (2.0f * a);
+        if (dist > 0) {
+            hitpoint.hit = true;
+            hitpoint.distance = dist;
+            hitpoint.hit_p = ray.origin + dist * ray.direction;
+            hitpoint.normal = glm::normalize(hitpoint.hit_p - point);
+            hitpoint.color_obj = color_;
+            hitpoint.name_obj = getName();
+            //std::cout << "Hit found at distance: " << dist << std::endl;
+        }
+        else {
+            //std::cout << "Hit behind ray origin" << std::endl;
+        }
+    }
+    else {
+        //std::cout << "No hit found" << std::endl;
+    }
 
-		}
-		hitpoint.ray_dir = ray.direction;
-		return hitpoint;
-	}
-	else {
-		Hitpoint hitpoint;
-		hitpoint.hit = false;
-		return hitpoint;
-	}*/
-
-	//hinzugefügt
-	if (glm::intersectRaySphere(ray.origin, v, point, rad * rad, dist)) {
-		Hitpoint hitpoint;
-		hitpoint.hit = true;
-		hitpoint.distance = dist;
-		hitpoint.name_obj = name_;
-		hitpoint.color_obj = color_;
-
-		// Berechne den Schnittpunkt und die Normale
-		glm::vec3 intersection_point = ray.origin + dist * ray.direction;
-		hitpoint.hit_p = intersection_point;
-		hitpoint.normal = glm::normalize(hitpoint.hit_p - point);
-
-		// Berechne Reflexion
-		hitpoint.reflection_dir = glm::reflect(ray.direction, hitpoint.normal);
-
-		// Berechne Brechung
-		float eta = 1.0f / hitpoint.refraction_index; // Annahme: Das Medium hat einen Brechungsindex
-		hitpoint.refraction_dir = glm::refract(ray.direction, hitpoint.normal, eta);
-
-		return hitpoint;
-	}
-	else {
-		return Hitpoint(); // Keine Kollision
-	}
-	//hinzugefügt ende
+    return hitpoint;
 }
--- framework/sphere.hpp
+++ framework/sphere.hpp
@@ -1,23 +1,26 @@
+// sphere.hpp
 #ifndef SPHERE_HPP
 #define SPHERE_HPP
 #include "shape.hpp"
 #include <glm/vec3.hpp>
 #include "Hitpoint.hpp"
 #include "Ray.hpp"
-#include <Hitpoint.hpp>
 
 class Sphere : public Shape {
 private:
-	float rad{ 10.0f };
-	glm::vec3 point{ 100.0f, 100.0f, 100.0f };
+    /*float rad{ 10.0f };
+    glm::vec3 point{ 100.0f, 100.0f, 100.0f };*/
 public:
-	Sphere();
-	~Sphere();
-	Sphere(float r, glm::vec3 p);
-	Sphere(float r, glm::vec3 p, std::string name, std::shared_ptr<Material> color);
-	float Shape::area()const override;
-	float Shape::volume()const override;
-	std::ostream& print(std::ostream& os)const;
-	Hitpoint intersect(Ray const& ray) override;
+    Sphere();
+    ~Sphere();
+    Sphere(float r, glm::vec3 p);
+    Sphere(float r, glm::vec3 p, std::string const& name, std::shared_ptr<Material> const& color);
+    float area() const override;
+    float volume() const override;
+    std::ostream& print(std::ostream& os) const override;
+    float rad{ 10.0f };
+    glm::vec3 point{ 100.0f, 100.0f, 100.0f };
+protected:
+    Hitpoint intersectImpl(Ray const& ray) const override;
 };
 #endif
