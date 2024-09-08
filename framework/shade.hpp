12aa3fbee060e9b8d415e2cacb616486c0aed3d1
SZ <sven.zschunke@web.de>
2024-09-08 12:50:02 +0200

    erstellen einer shade header für die Berechnung von Spiegelung/Reflexion, Refraction ect. um die Rendere Klasse zu entlasten und um Modularität des Codes zu fördern

--- framework/shade.hpp
+++ framework/shade.hpp
@@ -11,13 +11,25 @@
 
 class Shading {
 public:
-    Hitpoint compute_intersection(Ray const& ray, std::vector<std::shared_ptr<Shape>> const& shapes);
-    Color compute_lighting(Hitpoint const& hitpoint, Ray const& ray, std::vector<std::shared_ptr<Shape>> const& shapes, std::vector<std::shared_ptr<Light>> const& lights, int depth);
+    //Hitpoint compute_intersection(Ray const& ray, std::vector<std::shared_ptr<Shape>> const& shapes);
+    Color compute_lighting(const Hitpoint& hitpoint, const Ray& ray,
+        const std::shared_ptr<Shape>& root_shape,
+        const std::vector<std::shared_ptr<Light>>& lights, int depth) const;
+
+    // Fügen Sie diese Methode hinzu, um die Schnittpunktberechnung zu handhaben
+    Hitpoint compute_intersection(Ray const& ray, const std::shared_ptr<Shape>& root_shape) const;
+
+    static Color blend_colors(Color c1, Color c2, float weight1, float weight2);
 
 private:
     Color compute_diffuse(Hitpoint const& hitpoint, Light const& light);
     Color compute_specular(Hitpoint const& hitpoint, Light const& light, Ray const& ray);
-    Color blend_colors(Color c1, Color c2, float weight1, float weight2);
+    //Color blend_colors(Color c1, Color c2, float weight1, float weight2);
+
+
+    
+
+   
 };
 
 #endif // BUW_SHADING_HPP
